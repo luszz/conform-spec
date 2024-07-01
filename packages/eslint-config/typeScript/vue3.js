9c2c3f4ffa2578803cdae0cda656fa4dd3fa430e
luszz11 <luszz11@163.com>
2024-07-01 17:04:35 +0800

    fix: 修复eslint-config-conform jsx 解析问题

--- packages/eslint-config/typeScript/vue3.js
+++ packages/eslint-config/typeScript/vue3.js
@@ -1,10 +1,10 @@
 module.exports = {
-  extends: [
-    './index',
-    '../rules/vue3',
-  ].map(require.resolve),
+  extends: ['./index', '../rules/vue3'].map(require.resolve),
   parserOptions: {
     // https://github.com/mysticatea/vue-eslint-parser#parseroptionsparser
     parser: '@typescript-eslint/parser',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
 };
